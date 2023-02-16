a03ac489d83a65bab12c2c8b84b640728987547c
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-16 10:53:19 +0900

    setting the background color

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-// function random(min, max) {
-//     return min + Math.random() * (max + 1 - min);
-// }
-
-// const body = document.querySelector('body');
-// const canvasSize = body.offsetWidth * body.offsetHeight;
-// const starsFraction = canvasSize / 2000;
-
-// for (let i = 0; i < starsFraction; i++) {
-//     // Set up random elements
-//     let xPos = random(0, 100);
-//     let yPos = random(0, 100);
-//     let alpha = random(0.5, 1);
-//     let size = random(1, 2);
-//     let colour = '#f5f5f5';
-
-//     // Add them to the body
-//     const star = document.createElement('div');
-//     star.style.position = 'fixed';
-//     star.style.left = xPos + '%';
-//     star.style.top = yPos + '%';
-//     star.style.opacity = alpha;
-//     star.style.width = size + 'px';
-//     star.style.height = size + 'px';
-//     star.style.backgroundColor = colour;
-//     document.body.appendChild(star);
-// }
-
 ready(function () {
     initializeBackground();
 });
@@ -45,7 +17,6 @@ function initializeBackground() {
         }
 
         lastResizeTime = Date.now();
-        canvas.style.display = "none";
         resizeTimeout = setTimeout(function () {
             fadeIn(canvas, 500);
             initializeStars();
