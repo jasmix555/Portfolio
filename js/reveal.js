9659d1595e80eac2fbe262d14dc55d362fefbac3
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-02-17 01:10:02 +0900

    setting custom cursor

--- js/reveal.js
+++ js/reveal.js
@@ -50,3 +50,26 @@ function navHighlighter() {
         }
     });
 }
+
+let innerCursor = document.querySelector(".inner-cursor");
+
+document.addEventListener("mousemove" , moveCursor);
+
+function moveCursor(e){
+    let x = e.clientX;
+    let y = e.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+links.forEach((link) => {
+    link.addEventListener("mouseover", ()=>{
+        innerCursor.classList.add("grow")
+    })
+    link.addEventListener("mouseleave", ()=>{
+        innerCursor.classList.remove("grow")
+    })
+});
